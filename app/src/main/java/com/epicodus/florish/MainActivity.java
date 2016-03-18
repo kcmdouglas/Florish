687ef3af1a6225e4cc42b50dbd5440226061977a
kcmdouglas <kcmdouglas@gmail.com>
2016-03-18 14:36:46 -0700

    Add color palette, route buttons on main activity

--- app/src/main/java/com/epicodus/florish/MainActivity.java
+++ app/src/main/java/com/epicodus/florish/MainActivity.java
@@ -1,7 +1,11 @@
 package com.epicodus.florish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -9,9 +13,10 @@ import com.squareup.picasso.Picasso;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.background_main) ImageView mBackground;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.seasonalButton) Button mSeasonalButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Picasso.with(MainActivity.this)
-                .load(R.drawable.sunflower_background)
-                .fit()
-                .centerCrop()
-                .into(mBackground);
+        mLoginButton.setOnClickListener(this);
+        mSeasonalButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mLoginButton) {
+            Intent intent = new Intent(MainActivity.this, UserLoginActivity.class);
+            startActivity(intent);
+        } else if (v == mSeasonalButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, SeasonalActivity.class);
+            startActivity(intent);
+        }
     }
 }
