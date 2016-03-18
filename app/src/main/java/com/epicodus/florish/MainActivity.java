fc32e228c9b366958bd8385b011b24241a44f0d4
kcmdouglas <kcmdouglas@gmail.com>
2016-03-18 12:47:22 -0700

    Add activities and background for main activity

--- app/src/main/java/com/epicodus/florish/MainActivity.java
+++ app/src/main/java/com/epicodus/florish/MainActivity.java
@@ -2,12 +2,27 @@ package com.epicodus.florish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.background_main) ImageView mBackground;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Picasso.with(MainActivity.this)
+                .load(R.drawable.sunflower_background)
+                .fit()
+                .centerCrop()
+                .into(mBackground);
     }
 }
