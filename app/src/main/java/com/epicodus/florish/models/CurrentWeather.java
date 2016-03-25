d0dff013eb3e3667f1d8f5a75c58f878b7daeee3
kcmdouglas <kcmdouglas@gmail.com>
2016-03-25 14:24:50 -0700

    Add files for OpenWeatherMap service and CurrentWeather model

---
+++ app/src/main/java/com/epicodus/florish/models/CurrentWeather.java
@@ -0,0 +1,7 @@
+package com.epicodus.florish.models;
+
+/**
+ * Created by Kassidy on 3/25/2016.
+ */
+public class CurrentWeather {
+}
---
+++ app/src/main/java/com/epicodus/florish/service/OpenWeatherMapService.java
@@ -0,0 +1,7 @@
+package com.epicodus.florish.service;
+
+/**
+ * Created by Kassidy on 3/25/2016.
+ */
+public class OpenWeatherMapService {
+}
