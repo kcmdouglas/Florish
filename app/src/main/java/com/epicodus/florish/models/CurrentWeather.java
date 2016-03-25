6f156e0e888e76bc3dd7813015e0671d0ae027d2
kcmdouglas <kcmdouglas@gmail.com>
2016-03-25 14:59:25 -0700

    Add API calls for both Current Weather and Forecast

--- app/src/main/java/com/epicodus/florish/models/CurrentWeather.java
+++ app/src/main/java/com/epicodus/florish/models/CurrentWeather.java
@@ -2,52 +2,9 @@ package com.epicodus.florish.models;
 
 import android.content.Context;
 
-import com.epicodus.florish.R;
-
 /**
  * Created by Kassidy on 3/25/2016.
  */
 public class CurrentWeather {
-    private Context mContext;
-
-
-    public CurrentWeatherService(Context context) {
-        this.mContext = context;
-    }
-
-
-    public void findForecast(String city, Callback callback) {
-        String appid = mContext.getString(R.string.weather_API_key);
-
-        OkHttpClient client = new OkHttpClient.Builder().build();
-
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api.openweathermap.org/data/2.5/forecast?&units=imperial&APPID=" + appid).newBuilder();
-        urlBuilder.addQueryParameter("q", city);
-        String url = urlBuilder.build().toString();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        Call call = client.newCall(request);
-        call.enqueue(callback);
-    }
-
-    public void findCurrentWeather(String city, Callback callback) {
-        String appid = mContext.getString(R.string.weather_API_key);
-
-        OkHttpClient client = new OkHttpClient.Builder().build();
-
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api.openweathermap.org/data/2.5/weather?&units=imperial&APPID=" + appid).newBuilder();
-        urlBuilder.addQueryParameter("q", city);
-        String url = urlBuilder.build().toString();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        Call call = client.newCall(request);
-        call.enqueue(callback);
-    }
 
 }
--- app/src/main/java/com/epicodus/florish/service/OpenWeatherMapService.java
+++ app/src/main/java/com/epicodus/florish/service/OpenWeatherMapService.java
@@ -1,7 +1,60 @@
 package com.epicodus.florish.service;
 
+import android.content.Context;
+
+import com.epicodus.florish.R;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by Kassidy on 3/25/2016.
  */
 public class OpenWeatherMapService {
+    private Context mContext;
+
+
+    public CurrentWeatherService(Context context) {
+        this.mContext = context;
+    }
+
+
+    public void findForecast(String city, Callback callback) {
+        String appid = mContext.getString(R.string.open_weather_map_API_key);
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api.openweathermap.org/data/2.5/forecast?&units=imperial&APPID=" + appid).newBuilder();
+        urlBuilder.addQueryParameter("q", city);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public void findCurrentWeather(String city, Callback callback) {
+        String appid = mContext.getString(R.string.open_weather_map_API_key);
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api.openweathermap.org/data/2.5/weather?&units=imperial&APPID=" + appid).newBuilder();
+        urlBuilder.addQueryParameter("q", city);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+
 }
