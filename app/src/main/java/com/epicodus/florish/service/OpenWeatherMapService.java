6493efcc7509cf2387b1eff9a078b0b32d44dffe
kcmdouglas <kcmdouglas@gmail.com>
2016-03-27 13:50:34 -0700

    Add method to process results in OpenWeatherMapService

--- app/src/main/java/com/epicodus/florish/service/OpenWeatherMapService.java
+++ app/src/main/java/com/epicodus/florish/service/OpenWeatherMapService.java
@@ -3,12 +3,19 @@ package com.epicodus.florish.service;
 import android.content.Context;
 
 import com.epicodus.florish.R;
+import com.epicodus.florish.models.CurrentWeather;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by Kassidy on 3/25/2016.
@@ -56,5 +63,34 @@ public class OpenWeatherMapService {
         call.enqueue(callback);
     }
 
+    public CurrentWeather processCurrentResults(Response response) {
+
+        CurrentWeather currentWeather;
+        currentWeather = null;
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject currentWeatherJSON = new JSONObject(jsonData);
+                Integer weatherId = currentWeatherJSON.getJSONArray("weather").getJSONObject(0).getInt("id");
+                String mainDescription = currentWeatherJSON.getJSONArray("weather").getJSONObject(0).getString("main");
+                String longDescription = currentWeatherJSON.getJSONArray("weather").getJSONObject(0).getString("description");
+                Double currentTemp = currentWeatherJSON.getJSONObject("main").getDouble("temp");
+                Double tempMin = currentWeatherJSON.getJSONObject("main").getDouble("temp_min");
+                Double tempMax = currentWeatherJSON.getJSONObject("main").getDouble("temp_max");
+                Double humidity = currentWeatherJSON.getJSONObject("main").getDouble("humidity");
+                Integer unconvertedDate = currentWeatherJSON.getInt("dt");
+                String iconId = currentWeatherJSON.getJSONArray("weather").getJSONObject(0).getString("icon");
+                String imageUrl = "http://openweathermap.org/img/w/" + iconId + ".png";
+
+                currentWeather = new CurrentWeather(weatherId, mainDescription, longDescription, currentTemp, tempMin, tempMax, humidity, unconvertedDate, imageUrl);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return currentWeather;
+    }
 
 }
