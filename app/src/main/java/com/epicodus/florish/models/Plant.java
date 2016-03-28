c9509eb53e2bb7dc37ea514452d8ab9ba3d783a2
kcmdouglas <kcmdouglas@gmail.com>
2016-03-27 17:08:40 -0700

    Implement weather service call and current weather UI on User Page Activity

--- app/src/main/java/com/epicodus/florish/models/Plant.java
+++ app/src/main/java/com/epicodus/florish/models/Plant.java
@@ -1,7 +1,41 @@
 package com.epicodus.florish.models;
 
+import org.parceler.Parcel;
+
 /**
  * Created by Kassidy on 3/27/2016.
  */
+@Parcel
 public class Plant {
+    public String mName;
+    public String mCategory;
+    public String mPlantingSpan;
+    public String mDescription;
+
+    public Plant() {
+
+    }
+
+    public Plant(String name, String category, String plantingSpan, String description) {
+        mName = name;
+        mCategory = category;
+        mPlantingSpan = plantingSpan;
+        mDescription = description;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getCategory() {
+        return mCategory;
+    }
+
+    public String getPlantingSpan() {
+        return mPlantingSpan;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
 }
--- app/src/main/java/com/epicodus/florish/ui/UserPageActivity.java
+++ app/src/main/java/com/epicodus/florish/ui/UserPageActivity.java
@@ -3,6 +3,10 @@ package com.epicodus.florish.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.epicodus.florish.R;
 import com.epicodus.florish.models.CurrentWeather;
@@ -11,12 +15,19 @@ import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 
+import butterknife.Bind;
+import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-public class UserPageActivity extends AppCompatActivity {
+public class UserPageActivity extends AppCompatActivity{
     private CurrentWeather mCurrentWeather;
+    @Bind(R.id.user_starter_list_text_view) TextView mStarterTextView;
+    @Bind(R.id.current_weather_image) ImageView mCurrentWeatherImageView;
+    @Bind(R.id.current_weather_text_view) TextView mCurrentWeatherTextView;
+    @Bind(R.id.current_temperature_text_view) TextView mCurrentTemperatureTextView;
+    @Bind(R.id.share_button) Button mShareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,15 @@ public class UserPageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         getCurrentWeather(location);
+
+
+
+
+
+        //mShareButton.setOnClickListener();
     }
+
+   // private OnClick
 
 
 
@@ -46,11 +65,11 @@ public class UserPageActivity extends AppCompatActivity {
                 UserPageActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-//                        Picasso.with(UserPageActivity.this)
-//                                .load(mCurrentWeather.getImageUrl())
-//                                .into(mCurrentWeatherImageView);
-//                        mCurrentDescription.setText(mCurrentWeather.getLongDescription());
-//                        mCurrentTemp.setText(mCurrentWeather.getCurrentTemp() + "°");
+                        Picasso.with(UserPageActivity.this)
+                                .load(mCurrentWeather.getImageUrl())
+                                .into(mCurrentWeatherImageView);
+                        mCurrentWeatherTextView.setText(mCurrentWeather.getLongDescription());
+                        mCurrentTemperatureTextView.setText(mCurrentWeather.getCurrentTemp() + "°");
                     }
                 });
             }
