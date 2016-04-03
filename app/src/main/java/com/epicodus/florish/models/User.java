7720b28d4698f0fbb3eb7a088f54ab8b48d3cf2c
kcmdouglas <kcmdouglas@gmail.com>
2016-04-03 16:32:12 -0700

    Add User Creation fragment.

--- app/src/main/java/com/epicodus/florish/models/User.java
+++ app/src/main/java/com/epicodus/florish/models/User.java
@@ -1,16 +1,66 @@
 package com.epicodus.florish.models;
 
+import org.parceler.Parcel;
+
 import java.util.ArrayList;
 
 /**
  * Created by Kassidy on 3/27/2016.
  */
+@Parcel
 public class User {
-    private String mName;
-    private Integer mZipCode;
-    private ArrayList<Plant> mPlantsToStart;
-    private ArrayList<Plant> mPlantsInGarden;
-    private String[] mPollinators;
+    String name;
+    ArrayList<Plant> plantsToStart;
+    ArrayList<Plant> plantsInGarden;
+    String[] pollinators;
+    String userId;
 
+    public User() {
 
+    }
+
+    public User(String name, String userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Plant> getPlantsToStart() {
+        return plantsToStart;
+    }
+
+    public void setPlantsToStart(ArrayList<Plant> plantsToStart) {
+        this.plantsToStart = plantsToStart;
+    }
+
+    public ArrayList<Plant> getPlantsInGarden() {
+        return plantsInGarden;
+    }
+
+    public void setPlantsInGarden(ArrayList<Plant> plantsInGarden) {
+        this.plantsInGarden = plantsInGarden;
+    }
+
+    public String[] getPollinators() {
+        return pollinators;
+    }
+
+    public void setPollinators(String[] pollinators) {
+        this.pollinators = pollinators;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
---
+++ app/src/main/java/com/epicodus/florish/ui/UserCreationFragment.java
@@ -0,0 +1,96 @@
+package com.epicodus.florish.ui;
+
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.epicodus.florish.FlorishApplication;
+import com.epicodus.florish.R;
+import com.epicodus.florish.models.User;
+import com.firebase.client.Firebase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class UserCreationFragment extends DialogFragment implements View.OnClickListener {
+    private Firebase mFirebaseRef;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mName;
+    private String mUserId;
+    private String mLocation;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+    @Bind(R.id.usernameEditText) EditText mUsernameEditText;
+    @Bind(R.id.zipcodeEditText) EditText mZipcodeEditText;
+
+
+    public UserCreationFragment() {
+        // Required empty public constructor
+    }
+
+    public static UserCreationFragment newInstance() {
+        return new UserCreationFragment();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_user_creation, container, false);
+        ButterKnife.bind(this, view);
+
+        mFirebaseRef = FlorishApplication.getAppInstance().getFirebaseRef();
+
+        mUserId = mFirebaseRef.getAuth().getUid();
+
+        mSharedPreferences = this.getContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+
+        return view;
+    }
+
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle saveInstanceState) {
+        super.onViewCreated(view, saveInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSubmitButton) {
+            String name = mUsernameEditText.getText().toString();
+            String location = mZipcodeEditText.getText().toString();
+            addToSharedPreferences(location);
+            addToSharedPreferences(name);
+
+            createUser(name, mUserId);
+
+            dismiss();
+
+        }
+
+    }
+
+    private void addToSharedPreferences(String username) {
+        mEditor.putString("username", username).commit();
+    }
+
+    private void createUser(String name, String userId) {
+        User user = new User(name, userId);
+        mFirebaseRef.child("users/" + userId).push().setValue(user);
+    }
+}
