289d222a8ae9ab549b3aec58ad9243eded398bfa
kcmdouglas <kcmdouglas@gmail.com>
2016-04-04 15:27:08 -0700

    Add logout functionality.

--- app/src/main/java/com/epicodus/florish/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/florish/ui/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.SharedPreferences;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -88,4 +91,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String tag = "fragment_user_creation";
         userCreation.show(fm, tag);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                this.logout();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mFirebaseRef.unauth();
+        goToLoginActivity();
+    }
+
 }
